5260bccdfa4cdc20db747697cad334eb78293851
SaraAli22 <sarasalem48@yahoo.com>
2021-04-27 07:09:34 +0300

    edit

---
+++ Js/app.js
@@ -0,0 +1,23 @@
+
+var username = prompt("Please Enter your name").toUpperCase();
+alert ("Welcome"+username);
+
+let myname = confirm('Is my name Sara ?');
+alert('my name is Sara')
+console.log(myname)
+
+let myhome = confirm('do I live in Irbid ?');
+alert('I live in Irbid')
+console.log(myhome)
+
+let mymajor = confirm('Am I a doctor ?');
+alert('I am A civil Engineer')
+console.log(mymajor)
+
+let mydrink = confirm('Is Tea my Favourite drink ?');
+alert('my favourite drink is coffe')
+console.log(mydrink)
+
+let myhobby = confirm('Do I like Taking Picture ?');
+alert('I love to Take Pictures')
+console.log(myhobby)
