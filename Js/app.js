cf21f37df4b454420fc70aab347450fadbb47176
SaraAli22 <sarasalem48@yahoo.com>
2021-04-28 07:55:18 +0300

    edit

--- Js/app.js
+++ Js/app.js
@@ -1,24 +1,121 @@
 
+'use strict';
+var score = 0;
 var username = prompt("Please Enter your name").toUpperCase();
 alert ("Welcome"+username);
+alert('let\'s play a guessing game about me.');
+
+function yesCorrect (question) {
+    if (question.toUpperCase() === 'YES') {
+        score++;
+        alert("Yes!, you are right.");
+    }
+    else if (question.toUpperCase() === 'NO'){
+        alert("Sorry, wrong answer.");
+    }
+    else {
+        alert("This answer is not valid");
+    }
+}
+
+function noCorrect (question) {
+    if (question.toUpperCase() === 'YES') {
+        alert("Sorry, wrong answer.");
+    }
+    else if (question.toUpperCase() === 'NO'){
+        score++;
+        alert("Yes! you are right.");
+    }
+    else {
+        alert("This answer is not valid");
+    }
+}
 
 let myname = prompt('Is my name Sara ? yes or no ');
-alert('my name is Sara')
 console.log(myname)
+yesCorrect(myname);
 
 let myhome = prompt('do I live in Irbid ? yes or no');
-alert('I live in Irbid')
 console.log(myhome)
+yesCorrect(myhome);
 
 let mymajor = prompt('Am I a doctor ? yes or no ');
-alert('I am A civil Engineer')
 console.log(mymajor)
+noCorrect(mymajor);
 
 let mydrink = prompt('Is Tea my Favourite drink ? yes or no');
 alert('my favourite drink is coffe')
 console.log(mydrink)
+noCorrect(mydrink);
+
 
 let myhobby = prompt('Do I like Taking Picture ? yes or no ');
-alert('I love to Take Pictures')
 console.log(myhobby)
+yesCorrect(myhobby);
+
+
+
+
+
+for (var i = 1; i <= 4; i++ ) 
+{
+    var userNum = Number(prompt('What is The sum of first five prime numbers ?'));
+if (userNum == 28 ) {
+    alert('Great That is Correct');
+} else  
+    alert ('Too close')
+
+}
+function guessSum() {
+    var counter = 1;
+    if (counter === 5) {
+        alert('Sorry you run out of guesses');
+    }
+    while (counter < 5 && number !==28) {
+        var number = parseInt(prompt('What is The sum of first five prime numbers ?'));
+        if (number > 28) {
+            alert('Too high! Try Again');
+            counter++;
+        } else if (number < 28) {
+            alert('Too low! Try again');
+            counter++;
+        }
+    }
+    if (number === 28) {
+        alert('Great , That is Right');
+        score++;
+        counter = 5;
+    }
+}
+
+guessSum();
+
+
+function FavColor() {
+    var myFavcolor = ['black', 'green', 'blue', 'white'];
+
+    for (var i = 0; i < 6; i++) {
+        var useranswer = prompt('What is my favorite color?');
+        var answer = useranswer.toLowerCase();
+
+        for (var j = 0; j < myFavcolor.length; j++){
+            if (answer === myFavcolor[j]) {
+                alert('Congrats you got it right!!!');
+                score++;
+                i = 6;
+                break;
+            }
+        }
+        if (i !==6){
+            alert('Sorry wrong answer, try again');
+        }
+    }
+}
+
+FavColor();
+
+alert('My favorite colors are: black, green, blue, white');
+
+alert('Good Job!... You got ' + score + ' correct answers!');
+
 alert (" Thanks for your time"+ username)
